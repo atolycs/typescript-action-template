8b871adc3829e2ff2c0c6473fa086ae529371ebd
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-01-07 09:57:26 +0900

    init: test code

---
+++ src/wait.ts
@@ -0,0 +1,11 @@
+import { throws } from "assert";
+
+export async function wait(milliseconds: number): Promise<string> {
+  return new Promise((resolve) => {
+    if (Number.isNaN(milliseconds)) {
+      throw new Error("milliseconds not a number");
+    }
+
+    setTimeout(() => resolve("done"), milliseconds);
+  });
+}
